70a503a8b4eeeb081adff25a80e0281bd560454c
Pham <Stephen.Pham@lw.com>
2018-04-16 13:33:54 -0700

    Add 'real' codes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 
+// https://coursetro.com/posts/code/94/Use-Angular-with-Google's-Cloud-Firestore---Tutorial
+var firebaseConfig = {
+  apiKey: "AIzaSyAzG8QoxZ35sLBq-yO-hssz2EX2STnJ0uU",
+  authDomain: "employees-4fee4.firebaseapp.com",
+  databaseURL: "https://employees-4fee4.firebaseio.com",
+  projectId: "employees-4fee4",
+  storageBucket: "employees-4fee4.appspot.com",
+  messagingSenderId: "558184835678"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
